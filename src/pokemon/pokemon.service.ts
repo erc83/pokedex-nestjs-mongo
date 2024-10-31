3777094d1f6955d4c6a1b8dfb515e53a6fede43c
erc83 <espinoza.eric@gmail.com>
2024-10-31 02:49:10 -0300

    add: create pokemon in DB

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
-
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+
+  constructor(
+    @InjectModel( Pokemon.name )    //necesita el nombre del modelo
+    private readonly pokemonModel: Model<Pokemon>
+  ){}
+
+  // como es asincrona la insercion a la DB colocamos el async
+  async create(createPokemonDto: CreatePokemonDto) {
 
       createPokemonDto.name = createPokemonDto.name.toLowerCase()
 
-      return createPokemonDto
+      const pokemon = await this.pokemonModel.create( createPokemonDto  )
+
+      return pokemon
   }
 
   findAll() {
