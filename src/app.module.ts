ede0147933b2aaf088ca98442c3fdca7536a83f4
erc83 <espinoza.eric@gmail.com>
2024-11-17 01:58:54 -0300

    add: variable entorno, @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,24 @@ import { PokemonModule } from './pokemon/pokemon.module'
 import { MongooseModule } from '@nestjs/mongoose'
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config'
 
 
 @Module({
   imports: [
+    
+    ConfigModule.forRoot(),       // influye en la posicion
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27019/nest-pokemon'), // unique forRoot
-    PokemonModule, CommonModule, SeedModule
+    MongooseModule.forRoot( process.env.MONGO_DB), // unique forRoot
+    PokemonModule, CommonModule, SeedModule,
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(process.env)
+  }
+}
